5e6f297c979f1520ae12846787fe3c1fcedfa57f
Galvins <68190778+Galvin0705@users.noreply.github.com>
2021-09-08 07:11:36 +0700

    ok

---
+++ wsender.js
@@ -0,0 +1,46 @@
+const readline = require('readline');
+const { Webhook, MessageBuilder } = require('discord-webhook-node');
+const config = require("./config.json");
+
+
+
+const webhook = new Webhook(config.webhook_url);
+const webhook_avatar = config.httpavatar;
+webhook.setUsername(config.webhook_username);
+webhook.setAvatar(config.webhook_avatar);
+
+
+function main() {
+  return new Promise(function(resolve, reject) {
+    let rl = readline.createInterface(process.stdin, process.stdout)
+	console.log("Super Simple webhook executor written on NodeJS (C)\n")
+	
+    rl.setPrompt(`SendMessage@${config.webhook_username}: `);
+    rl.prompt();
+    rl.on('line', function(command, error) {
+      if (command === "!exit" || command === "!quit" || command == '!q') {
+        console.log('bye')
+      process.exit(1)
+      } if (command == "!clear"){
+		  console.clear();
+	  }else {
+		  try {
+        webhook.send(command)
+		  } catch (err){
+			  return console.log("error goblog");
+		  }
+      }
+      rl.prompt()
+
+    })
+  })
+}
+
+function run() {
+  try {
+    main()
+  } catch(err) {
+	  return console.log("Something went wrong.");
+  }
+}
+run()
