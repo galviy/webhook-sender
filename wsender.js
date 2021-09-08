50a21d353e8a0eeb62b9a25c79dc6a088cd654be
Galvins <68190778+Galvin0705@users.noreply.github.com>
2021-09-08 10:36:23 +0700

    Update wsender.js

--- wsender.js
+++ wsender.js
@@ -1,5 +1,8 @@
 const readline = require('readline');
-const { Webhook, MessageBuilder } = require('discord-webhook-node');
+const {
+    Webhook,
+    MessageBuilder
+} = require('discord-webhook-node');
 const config = require("./config.json");
 
 
@@ -11,36 +14,41 @@ webhook.setAvatar(config.webhook_avatar);
 
 
 function main() {
-  return new Promise(function(resolve, reject) {
-    let rl = readline.createInterface(process.stdin, process.stdout)
-	console.log("Super Simple webhook executor written on NodeJS (C)\n")
-	
-    rl.setPrompt(`SendMessage@${config.webhook_username}: `);
-    rl.prompt();
-    rl.on('line', function(command, error) {
-      if (command === "!exit" || command === "!quit"') {
-        console.log('bye')
-      process.exit(1)
-      } if (command == "!clear"){
-		  console.clear();
-	  }else {
-		  try {
-        webhook.send(command)
-		  } catch (err){
-			  return console.log("error goblog");
-		  }
-      }
-      rl.prompt()
+    return new Promise(function(resolve, reject) {
+        let rl = readline.createInterface(process.stdin, process.stdout)
+        console.log("Super Simple webhook executor written on NodeJS (C)\n")
 
+        rl.setPrompt(`SendMessage@${config.webhook_username}: `);
+        rl.prompt();
+        rl.on('line', function(command, error) {
+            if (command === "!exit" || command === "!quit" || command == '!q') {
+                console.log('bye')
+                process.exit(1)
+            }
+            if (command == "!clear") {
+                console.clear();
+            } else {
+                if (!command) {
+                    console.log(`please enter the message`)
+                } else {
+                    try {
+                        webhook.send(command)
+                    } catch (err) {
+                        return console.log("error goblog");
+                    }
+                }
+            }
+            rl.prompt()
+
+        })
     })
-  })
 }
 
 function run() {
-  try {
-    main()
-  } catch(err) {
-	  return console.log("Something went wrong.");
-  }
+    try {
+        main()
+    } catch (err) {
+        return console.log("Something went wrong.");
+    }
 }
 run()
